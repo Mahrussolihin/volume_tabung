9eae410e44e8558ad57d496149887fa3a8f42c5e
Mahrussolihin <143859887+Mahrussolihin@users.noreply.github.com>
2023-09-04 01:09:52 +0700

    Add files via upload
    coding sederhana untuk menghitung volume tabung

---
+++ volume_tabung.py
@@ -0,0 +1,41 @@
+print("Menghitung Volume Tabung")
+print("========================")
+
+print("Nama = Mahrus Solihin")
+print("NIM = 230441100032")
+print()
+
+#coding untuk mengetahui tipe datanya
+nama = "Mahrus Solihin"
+print(nama, "Tipenya", type(nama))
+NIM = 230441100032
+print(NIM, "Tipenya", type(NIM))
+print()
+
+print("Diketahui")
+print("phi = 22/7")
+print("Jari - jari tabung = 14")
+print("Tinggi tabung = 26")
+print()
+
+#coding untuk mengetahui tipe datanya
+phi = 22/7
+print(phi, "Tipenya", type(phi))
+jari_jari_tabung = 14
+print(jari_jari_tabung, "Tipenya", type(jari_jari_tabung))
+tinggi_tabung = 26
+print(tinggi_tabung, "Tipenya", type(tinggi_tabung))
+print()
+
+#Rumus volume tabung
+print("Rumus volume tabung = phi*r*r*t")
+print("phi*r*r*t = 22/7*14*14*26")
+
+phi = 22/7
+r = 14
+t = 26
+volume = phi*r*r*t
+
+print()
+print("----------Hasilnya----------")
+print("volume tabung adalah = ", format(volume))
